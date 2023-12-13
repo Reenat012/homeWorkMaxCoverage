57ba8d0d129615895bf36a83ca9d5e3440b83589
Reenat <reenat056@yandex.ru>
2023-12-13 18:15:21 +0300

    change

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,62 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
+    val typeCard = "MasterCard"
+    val sumRemittanceMonthAgo = 3_000_00
+    val sumRemittanceNew = 120_000
+    printSumRemmittance(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) //расчет комиссии
 }
+
+//проверяем входит ли сумма переводов в лимит в сутки
+fun isExceedLimitDay(typeCard: String, sumRemittanceNew: Int): Boolean {
+    return when (typeCard) {
+        "MasterCard", "Maestro", "Visa", "Мир" -> if (sumRemittanceNew in 0..150_000) true else false
+        "VK Pay" -> if (sumRemittanceNew in 0..15_000) false else true
+        else -> false
+    }
+}
+
+//проверяем входит ли сумма переводов в лимит в месяц
+fun isExceedLimitMonth(typeCard: String, sumRemittanceMonthAgo: Int, sumRemittanceNew: Int): Boolean {
+    return when (typeCard) {
+        "MasterCard", "Maestro", "Visa", "Мир" -> if (sumRemittanceMonthAgo + sumRemittanceNew <= 600_000) true else false
+        "VK Pay" -> if (sumRemittanceMonthAgo + sumRemittanceNew <= 40_000) true else false
+        else -> false
+    }
+}
+
+//рассчитываем сумму комиссии, если превышены лимиты возвращает -1 или -2
+fun calcCommission(typeCard: String = "VK Pay", sumRemittanceMonthAgo: Int = 0, sumRemittanceNew: Int): Int {
+    return if (isExceedLimitDay(typeCard, sumRemittanceNew)) {
+        if (isExceedLimitMonth(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)) {
+            when (typeCard) {
+                "MasterCard", "Maestro" -> if ((sumRemittanceMonthAgo + sumRemittanceNew) in 300..75_000) 0
+                else (sumRemittanceNew * 0.006).toInt() + 20
+
+                "Visa", "Мир" -> if ((sumRemittanceNew * 0.0075) < 35) 35
+                else (sumRemittanceNew * 0.0075).toInt()
+
+                else -> 0
+            }
+        } else return -1
+    } else -2
+}
+
+//суммируем разовый перевод и комиссию, если вывод -1 или -2 исчерпаны лимиты
+fun sumRemittanceWithCommision(
+    typeCard: String = "VK Pay",
+    sumRemittanceMonthAgo: Int = 0,
+    sumRemittanceNew: Int
+): Int {
+    return if (calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -1) -1
+    else if (calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -2) -2
+    else sumRemittanceNew + calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)
+}
+
+//вывод перевода с комиссией, с условием превышения лимита
+fun printSumRemmittance(typeCard: String, sumRemittanceMonthAgo: Int, sumRemittanceNew: Int) {
+    if (sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -1)
+        println("Лимит переводов в этом месяце исчерпан")
+    else if (sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew) == -2)
+        println("Лимит переводов в сутки исчерпан")
+    else println(sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew))
+}
+
--- src/test/kotlin/ru/netology/MainKtTest.kt
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -1,28 +1,56 @@
 package ru.netology
 
+import calcCommission
+import isExceedLimitDay
+import isExceedLimitMonth
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import printSumRemmittance
+import sumRemittanceWithCommision
 
 class MainKtTest {
 
     @Test
     fun isExceedLimitDay() {
+        val typeCard = "MasterCard"
+        val sumRemittanceNew = 120_000
+
+        val result = isExceedLimitDay(typeCard, sumRemittanceNew) //записываем в переменную результат вызова функции
+
+        assertEquals(true, result)
     }
 
     @Test
     fun isExceedLimitMonth() {
+        val typeCard = "MasterCard"
+        val sumRemittanceMonthAgo = 3_000_00
+        val sumRemittanceNew = 120_000
+
+        val result = isExceedLimitMonth(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)
+
+        assertEquals(true, result)
     }
 
     @Test
     fun calcCommission() {
+        val typeCard = "MasterCard"
+        val sumRemittanceMonthAgo = 3_000_00
+        val sumRemittanceNew = 120_000
+
+        val result = calcCommission(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)
+
+        assertEquals(740, result)
     }
 
     @Test
     fun sumRemittanceWithCommision() {
-    }
+        val typeCard = "MasterCard"
+        val sumRemittanceMonthAgo = 3_000_00
+        val sumRemittanceNew = 120_000
 
-    @Test
-    fun printSumRemmittance() {
+        val result = sumRemittanceWithCommision(typeCard, sumRemittanceMonthAgo, sumRemittanceNew)
+
+        assertEquals(120740, result)
     }
 }
