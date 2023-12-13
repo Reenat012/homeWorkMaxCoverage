d141527948af07e30dcb72e5077f96c052de01e1
Reenat <reenat056@yandex.ru>
2023-12-13 17:38:55 +0300

    add all

---
+++ src/test/kotlin/ru/netology/MainKtTest.kt
@@ -0,0 +1,28 @@
+package ru.netology
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class MainKtTest {
+
+    @Test
+    fun isExceedLimitDay() {
+    }
+
+    @Test
+    fun isExceedLimitMonth() {
+    }
+
+    @Test
+    fun calcCommission() {
+    }
+
+    @Test
+    fun sumRemittanceWithCommision() {
+    }
+
+    @Test
+    fun printSumRemmittance() {
+    }
+}
